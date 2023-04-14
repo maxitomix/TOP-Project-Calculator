aab9ba99c4454a67d9706425fa29ab9880d63c9c
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-04-14 11:47:19 -0400

    numbers working

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-let totalToDisplay = "0"
-
-
 const displayScreen = document.querySelector('.display');
+const btnNumbers = document.querySelectorAll('.btn-numbers');
+
+
+let totalToDisplay = "0"
+let clickCounter = 0;
 
 function updateDisplay(){
     displayScreen.textContent = totalToDisplay;
+    }
+
+
+function buttonNumbers(event){
+    const Element = event.target;
+    if (clickCounter>9)
+    {
+        clickCounter=1;  
+        totalToDisplay = "-ERROR- Only 9 digits at a time...!"
+        updateDisplay()
+        totalToDisplay = ""
+    }
+    else
+    {
+        totalToDisplay += Element.textContent;
+        updateDisplay()
+        console.log(Element.textContent)
+        clickCounter++
+        console.log(clickCounter)
+    }
+
 }
+
+
+// Code
+
 updateDisplay()
 totalToDisplay = ""
 
-const btnNumbers = document.querySelectorAll('.btn-numbers');
-let clickCounter = 0;
 btnNumbers.forEach((Element) => {
-    Element.addEventListener('click', ()=>{
-        
-            if (clickCounter>9)
-            {
-                clickCounter=1;  
-                totalToDisplay = "-ERROR- Only 9 digits at a tine...!"
-                updateDisplay()
-                totalToDisplay = ""
-            }
-            else
-            {
-                totalToDisplay += Element.textContent;
-                updateDisplay()
-                console.log(Element.textContent)
-                clickCounter++
-                console.log(clickCounter)
-            }
-
-        
-    })
+    Element.addEventListener('click', buttonNumbers)
 });
