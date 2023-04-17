cb0d303f19f67fcb99b2866afac560320498bdb6
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-04-17 11:51:57 -0400

    new code

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ const btnDecimal = document.getElementById('button_Decimal');
 
 let totalToDisplay = "0"
 let clickCounter = 0;
-let accumulator = 0;
+let accumulator = undefined;
 let lastOperation = "start"
 let newInput = 0;
+let operand = "start"
+let total = 0;
+let equalToggle = 0 ;
 
 function updateDisplay(){
     displayScreen.textContent = totalToDisplay;
@@ -27,7 +30,7 @@ function allClear(){
     totalToDisplay = "0";
     clickCounter = 0;
     updateDisplay();
-    accumulator = 0;
+    accumulator = undefined;
     totalToDisplay = "";
     lastOperation = "Clear";
     console.log("last Operation:",lastOperation)
@@ -66,93 +69,141 @@ btnNumbers.forEach((Element) => {
     Element.addEventListener('click', buttonNumbers)
 });
 
-btnOperands.forEach((Element) => {
-    Element.addEventListener('click', () =>
-    {
-        console.log(Element.textContent)
-        clickCounter = 0;
-    })
-});
 
 btnClear.addEventListener('click',allClear);
 
-btnEqual.addEventListener('click',()=>{
-    pullDisplay();
-    console.log(totalToDisplay);
-    console.log(accumulator);
-    switch (lastOperation){
-        case "add": 
-                    pullDisplay();
-                    accumulator = accumulator + parseInt(totalToDisplay);
-                    break;
-        case "add": 
-                    pullDisplay();
-                    accumulator = accumulator - parseInt(totalToDisplay);
-                    break;
-    }   
+btnOperands.forEach((Element) => {
+    Element.addEventListener('click', () =>
+    {
+    console.log(Element.textContent);
+    operand = Element.textContent;
+    if (operand != "Clear")
+    {       
+            clickCounter = 0;
+            pullDisplay();
+            newInput = Math.abs(parseInt(totalToDisplay));
+            console.log("new input:",newInput);
+            console.log("operand:",operand);
+            console.log("last operation:",lastOperation);
+            if (equalToggle === 1)
+            {
+                accumulator = undefined;
+                equalToggle = 0;
+            }
 
-    totalToDisplay = accumulator;
-    updateDisplay();
-    lastOperation = "equal";
-    console.log("last Operation:",lastOperation)
-    
-});
+            if (operand === "=")
+            {
+                operand = lastOperation;
+                console.log("operand =:",operand);
+                equalToggle = 1;
+            }    
+            if (accumulator === undefined)
+            {
+                accumulator = newInput;
+                lastOperation = operand;
+            }
+            else
+            {
+                switch (operand)
+                {
+                    case "+": 
+                            accumulator = accumulator + newInput
+                            console.log("acumulator +",accumulator);
+                            lastOperation = "+";
+                            break;
 
-btnAdd.addEventListener('click',()=>{
-    console.log("Start Add. Last Operation and Acumulator:", lastOperation, accumulator)
-    if (lastOperation==="equal")
-        {
-            lastOperation = "add";
-            totalToDisplay = "0";
-            console.log("last Operation update:", lastOperation)
-            updateDisplay();
-            totalToDisplay = "";
+                    case "-": 
+                            accumulator = accumulator - newInput
+                            console.log("acumulator +",accumulator);
+                            lastOperation = "-";
+                            break;
+                }
+            }
+            totalToDisplay = accumulator;
+            updateDisplay()
+            totalToDisplay = ""
         }
-
-    pullDisplay();
-    console.log("display captured: ",totalToDisplay);
-    console.log("accumulator initial: ",accumulator);
-    newInput = Math.abs(parseInt(totalToDisplay));
-    accumulator = accumulator + newInput;
-    console.log("accumulator final: ",accumulator);
-    console.log("Total to Display: ",accumulator);
-    totalToDisplay = accumulator;
-    updateDisplay();
-    totalToDisplay = "";
-    lastOperation = "add";
-    console.log("last Operation update:",lastOperation)
-    console.log("End Add. Last Operation and Acumulator:", lastOperation, accumulator)
-
+    }) 
 });
 
-btnSubstract.addEventListener('click',()=>{
-    console.log("Start substarct. Last Operation and Acumulator:", lastOperation, accumulator)
-    if (lastOperation==="equal")
-        {
-            lastOperation = "substract";
-            totalToDisplay = "0";
-            console.log("last Operation update:", lastOperation)
-            updateDisplay();
-            totalToDisplay = "";
-        }
 
-    pullDisplay();
-    console.log("display captured: ",totalToDisplay);
-    console.log("accumulator initial: ",accumulator);
-    newInput = Math.abs(parseInt(totalToDisplay));
-    console.log("new input: ",newInput);
-    accumulator = accumulator - newInput;
-    console.log("accumulator final: ",accumulator);
-    console.log("Total to Display: ",accumulator);
-    totalToDisplay = accumulator;
-    updateDisplay();
-    totalToDisplay = "";
-    lastOperation = "substract";
-    console.log("last Operation update:",lastOperation)
+// btnEqual.addEventListener('click',()=>{
+//     pullDisplay();
+//     console.log(totalToDisplay);
+//     console.log(accumulator);
+//     switch (lastOperation){
+//         case "add": 
+//                     pullDisplay();
+//                     accumulator = accumulator + parseInt(totalToDisplay);
+//                     break;
+//         case "add": 
+//                     pullDisplay();
+//                     accumulator = accumulator - parseInt(totalToDisplay);
+//                     break;
+//     }   
+
+//     totalToDisplay = accumulator;
+//     updateDisplay();
+//     lastOperation = "equal";
+//     console.log("last Operation:",lastOperation)
+    
+// });
+
+// btnAdd.addEventListener('click',()=>{
+//     console.log("Start Add. Last Operation and Acumulator:", lastOperation, accumulator)
+//     if (lastOperation==="equal")
+//         {
+//             lastOperation = "add";
+//             totalToDisplay = "0";
+//             console.log("last Operation update:", lastOperation)
+//             updateDisplay();
+//             totalToDisplay = "";
+//         }
+
+//     pullDisplay();
+//     console.log("display captured: ",totalToDisplay);
+//     console.log("accumulator initial: ",accumulator);
+//     newInput = Math.abs(parseInt(totalToDisplay));
+//     accumulator = accumulator + newInput;
+//     console.log("accumulator final: ",accumulator);
+//     console.log("Total to Display: ",accumulator);
+//     totalToDisplay = accumulator;
+//     updateDisplay();
+//     totalToDisplay = "";
+//     lastOperation = "add";
+//     console.log("last Operation update:",lastOperation)
+//     console.log("End Add. Last Operation and Acumulator:", lastOperation, accumulator)
+
+// });
+
+// btnSubstract.addEventListener('click',()=>{
+//     console.log("Start substarct. Last Operation and Acumulator:", lastOperation, accumulator)
+//     if (lastOperation==="equal")
+//         {
+//             lastOperation = "substract";
+//             totalToDisplay = "0";
+//             console.log("last Operation update:", lastOperation)
+//             updateDisplay();
+//             totalToDisplay = "";
+//         }
+
+//     pullDisplay();
+//     console.log("display captured: ",totalToDisplay);
+//     console.log("accumulator initial: ",accumulator);
+//     newInput = Math.abs(parseInt(totalToDisplay));
+//     console.log("new input: ",newInput);
+//     accumulator = accumulator - newInput;
+//     console.log("accumulator final: ",accumulator);
+//     console.log("Total to Display: ",accumulator);
+//     totalToDisplay = accumulator;
+//     updateDisplay();
+//     totalToDisplay = "";
+//     lastOperation = "substract";
+//     console.log("last Operation update:",lastOperation)
     
 
 
 
-    console.log("End substarct. Last Operation and Acumulator:", lastOperation, accumulator)
+//     console.log("End substarct. Last Operation and Acumulator:", lastOperation, accumulator)
 
-});
+// });
